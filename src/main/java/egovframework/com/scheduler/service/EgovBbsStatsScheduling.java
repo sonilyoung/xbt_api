168e866b4325b6a9b27db324f37bf723c530b5fa
sonilyoung <iys4906@gmail.com>
2023-01-17 17:02:41 +0900

    쿼리 수정 및 기타 수정

--- src/main/java/egovframework/com/scheduler/service/EgovBbsStatsScheduling.java
+++ src/main/java/egovframework/com/scheduler/service/EgovBbsStatsScheduling.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 
 /**
- * 게시물 통계 집계를 위한 스케줄링 클래스
+ * 스케줄링 클래스
  * @author 공통서비스 개발팀 박지욱
  * @since 2009.04.16
  * @version 1.0
@@ -30,10 +30,12 @@ public class EgovBbsStatsScheduling extends EgovAbstractServiceImpl {
 
 
 	/**
-	 * 게시물 통계를 위한 집계를 하루단위로 작업하는 배치 프로그램
+	 * 하루단위로 작업하는 배치 프로그램
 	 * @exception Exception
 	 */
 	public void summaryBbsStats() throws Exception {
-		System.out.println("스케줄실행");
+		System.out.println("엠폴시스템 raw이미지 저장 인터페이스");
+		
+		
 	}
 }
